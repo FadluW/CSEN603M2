2eeb9cf699b1b8dbd9ad6354f8c0f5e5f8088cf6
FadluW <fadlu.w@gmail.com>
2023-05-23 18:05:53 +0200

    change login/registration

--- functions/addUser.js
+++ functions/addUser.js
@@ -6,37 +6,60 @@ module.exports =
     * Throws apropriate errors.
     * @param {*} username 
     * @param {*} password 
-    * @param {*} userType Client/Banker, defaults to Client
+    * @param {*} data additional user information such as
+    * userType: Client/Banker, defaults to Client
+    * natID: Client's national ID
     * @returns 
     */
-    function (username, password, userType = 'client') {
-       // Import DB
-       const Users = JSON.parse(fs.readFileSync(`./localDB/users.json`))     
+    function (username, password, data = {}) {
+        // Import DB
+        const Users = JSON.parse(fs.readFileSync(`./localDB/users.json`))     
    
-       // Ensure no empty field
-       if (!username || !password) {
+        // Ensure no empty field
+        if (!username || !password) {
            throw new Error('Please enter both the username and password.');
-       }
+        }
+
+        userType = data.userType ?? "client";
+        if (userType == "client" && data.natID == undefined) {
+            throw new Error('Please enter your national ID.')
+        }
    
-       // User already exists
-       if (Users[username]) {
-           throw new Error(`Username ${username} already taken!`);
-       }
+        // User already exists
+        if (Users[username]) {
+            throw new Error(`Username ${username} already taken!`);
+        }
    
-       // Add user to DB
-       Users[username] = {
+        // Add user to DB
+        Users[username] = {
            password: password,
            createdOn: Date.now(),
            type: userType
-       }
-       fs.writeFile(`./localDB/users.json`, JSON.stringify(Users), 'utf8', (err) => {
-           if (err) {
-               console.log(err);
-               throw new Error('Failed to save DB')
-           } else {
-               console.log(`Registered user: ${username}`)
-           }
-       })
+        }
+        // If client check if already owns accounts
+        if (userType == "client") {
+            Users[username].natID = data.natID;
+            const Accounts = JSON.parse(fs.readFileSync(`./localDB/accounts.json`))     
+
+            for (const curr in Accounts) {
+                if (Accounts[curr].natID == data.natID) {
+                    if (Users[username]?.accountIDs == undefined) {
+                        Users[username].accountIDs = [curr];
+                    } else {
+                        Users[username].accountIDs.push(curr);
+                    }
+                }
+            }
+        }
+
+        fs.writeFile(`./localDB/users.json`, JSON.stringify(Users), 'utf8', (err) => {
+            if (err) {
+                console.log(err);
+                throw new Error('Failed to save DB')
+            } else {
+                console.log(`Registered user: ${username}`)
+            }
+        })
    
        return
    }
